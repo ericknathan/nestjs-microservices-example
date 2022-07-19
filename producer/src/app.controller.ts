614140e6181a911441be2a0efaf3f39b941e6be0
ericknathan <erick.capito@hotmail.com>
2022-07-19 14:40:11 -0300

    feat [producer]: create route to send message

--- producer/src/app.controller.ts
+++ producer/src/app.controller.ts
@@ -1,12 +1,33 @@
 import { Controller, Get } from '@nestjs/common';
+import { Client, ClientKafka, Transport } from '@nestjs/microservices';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Client({
+    transport: Transport.KAFKA,
+    options: {
+      client: {
+        brokers: ['localhost:9093'],
+      },
+      consumer: {
+        groupId: 'ec-consumer',
+      },
+    },
+  })
+  client: ClientKafka;
+
+  async onModuleInit() {
+    this.client.subscribeToResponseOf('storage-ec');
+    await this.client.connect();
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  sendToStorage() {
+    return this.client.send('storage-ec', {
+      message: 'remove pizza from storage',
+    });
   }
 }
